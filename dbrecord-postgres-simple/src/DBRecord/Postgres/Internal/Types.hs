7abdcb05675c03f4b87d68fe6fb105918041579a
Sreenidhi <nair.sreenidhi@gmail.com>
2019-06-28 14:26:40 +0530

    interval

--- dbrecord-postgres-simple/src/DBRecord/Postgres/Internal/Types.hs
+++ dbrecord-postgres-simple/src/DBRecord/Postgres/Internal/Types.hs
@@ -14,3 +14,15 @@ import Data.ByteString (ByteString)
 import DBRecord.Internal.Types (DbK (..))
 import Data.Proxy
 
+import qualified DBRecord.Internal.PrimQuery as PQ
+import qualified Data.Text.Encoding as T
+
+instance FromField Interval where                                                                                                                    
+  fromField f Nothing   = returnError UnexpectedNull f ""
+  fromField f (Just val) = do                                                 
+    tName <- typename f
+    if tName == "interval"
+      then case T.decodeUtf8' val  of
+        Left err -> returnError ConversionFailed f (show err)
+        Right a -> return (Interval a)
+      else returnError Incompatible f ("Wrong database type for Interval, saw: " ++ show tName)
