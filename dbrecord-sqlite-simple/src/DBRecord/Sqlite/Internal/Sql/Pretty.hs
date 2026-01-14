8776f2088cba7151f099c039659e2cb18525c6ba
kavin_kumar03 <kavinkumar@byteally.com>
2026-01-14 18:31:42 +0530

    fixed combile error on sqlite

--- dbrecord-sqlite-simple/src/DBRecord/Sqlite/Internal/Sql/Pretty.hs
+++ dbrecord-sqlite-simple/src/DBRecord/Sqlite/Internal/Sql/Pretty.hs
@@ -211,7 +211,7 @@ ppColumn (SqlColumn s) =
 
 ppTableExpr :: SqlTableExpr -> Doc
 ppTableExpr (NestedSqlSelect sql)     = ppSelect sql
-ppTableExpr (SqlTabName sqltab)       = ppTableName sqltab
+ppTableExpr (SqlTabName sqltab _)       = ppTableName sqltab
 ppTableExpr (SqlTabFun funName args)  = ppTableFun funName args
 
 ppTableFun :: SqlName -> [SqlName] -> Doc
